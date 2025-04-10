58c978ea04777d1146fe2558ff8a1e5eb3a98714
Jaroslav Mejzlik <jarek850@seznam.cz>
2025-04-10 09:05:47 +0200

    01

--- searching.py
+++ searching.py
@@ -18,13 +18,26 @@ def read_data(file_name, field):
     with open(file_name, "r", encoding="utf8") as file:
         read = json.load(file)
         read = read[field]
-    return print(read)
+    return read
+
+def linear_search(sekv, number):
+    dict = {}
+    list_for_dummies = []
+    o = 0
+    for i, value in enumerate(sekv):
+        if number == value:
+            list_for_dummies.append(i)
+            dict["index"] = list_for_dummies
+            o += 1
+    dict["count"] = o
+    return print(dict)
+
 
 def main():
-
+    readed = read_data("sequential.json", "unordered_numbers")
+    linear_search(readed, 5)
     pass
 
 
 if __name__ == '__main__':
     main()
-    read_data("sequential.json", "unordered_numbers")
